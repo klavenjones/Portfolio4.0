633f8862b7b1a54a7c51c2d11abeb51bf45b5629
Klaven Jones <klavensjones@gmail.com>
2020-10-24 22:22:12 -0400

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+
+
+
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: ["./public/**/*.html"],
+  theme: {
+    fontFamily:{
+      sans: ['Montserrat', "sans-serif"]
+    },
+    extend: {
+        
+    },
+  },
+  variants: {},
+  plugins: [],
+}
