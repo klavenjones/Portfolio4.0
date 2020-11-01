3c3f8f73fdec10d85858cee5bb3c3a6e006007ca
Klaven Jones <klavensjones@gmail.com>
2020-10-31 22:11:14 -0400

    Finished Basic Design of Portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         fontSize: {
           'display': 'clamp(1.25rem, 5vw, 5rem)',
           'display-description': 'clamp(1.25rem, 5vw, 4rem)',
-          'display-footer': 'clamp(1rem, 5vw, 3.75rem)',
+          'display-footer': 'clamp(1.25rem, 5vw, 3.25rem)',
           'section-title-xl': '10rem',
           'section-title-lg': '8rem',
           'section-title-md': '5.8rem',
@@ -24,6 +24,20 @@ module.exports = {
         height: {
             '560': "35rem",
             '500': "31.25rem"
+        },
+        maxWidth: {
+            'xl-vw': '80vw',
+            '2xl-vw': '85vw',
+            
+        },
+        spacing:{
+            '72' : '18rem',
+            '80' : '20rem'
+
+        },
+        screens: {
+            '2xl': '1800px',
+            '3xl': '2000px'
         }
     },
   },
